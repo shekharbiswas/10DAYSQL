2a11294992da58cbb8131ae437e2819fa4eb20a1
shekharbiswas <biswas.berlin89@gmail.com>
2021-06-01 17:52:58 +0200

    updated

---
+++ SQL_exercise_01/1_questions.sql
@@ -0,0 +1,10 @@
+-- 1.1 Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).
+-- 1.2 Select the average price of each manufacturer's products, showing only the manufacturer's code.
+-- 1.3 Select the average price of each manufacturer's products, showing the manufacturer's name.
+-- 1.4 Select the names of manufacturer whose products have an average price larger than or equal to $150.
+-- 1.5 Select the name and price of the cheapest product.
+-- 1.6 Select the name of each manufacturer along with the name and price of its most expensive product.
+-- 1.7 Add a new product: Loudspeakers, $70, manufacturer 2.
+-- 1.8 Update the name of product 8 to "Laser Printer".
+-- 1.9 Apply a 10% discount to all products.
+-- 1.10 Apply a 10% discount to all products with a price larger than or equal to $120.
---
+++ SQL_exercise_09/9_questions.sql
@@ -0,0 +1,10 @@
+-- 9.1 Give the package name and how many times they're downloaded. Order by the 2nd column descently.
+-- 9.2 Give the package ranking (based on how many times it was downloaded) during 9AM to 11AM
+-- 9.3 How many recordings are from China ("CN") or Japan("JP") or Singapore ("SG")?
+-- 9.4 Print the countries whose downloaded are more than the downloads from China ("CN")
+-- 9.5 Print the average length of the package name of all the UNIQUE packages
+-- 9.6 Get the package whose downloading count ranks 2nd (print package name and it's download count).
+-- 9.7 Print the name of the package whose download count is bigger than 1000.
+-- 9.8 The field "r_os" is the operating system of the users.
+    -- 	Here we would like to know what main system we have (ignore version number), the relevant counts, and the proportion (in percentage).
+
